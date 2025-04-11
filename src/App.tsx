9b7a063f4c4d66b3e39bcf0d814ad73d8452cdda
Awais Ashraf <mhawais154@gmail.com>
2025-04-11 07:42:20 +0500

    feat(ui) make responsive simple layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import { Button, HStack } from "@chakra-ui/react";
+import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
+import NavBar from "./NavBar";
 
 function App() {
+  const shouldShowAside = useBreakpointValue({ base: false, lg: true });
+
   return (
-    <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar/>
+      </GridItem>
+
+      {shouldShowAside && (
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      )}
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
